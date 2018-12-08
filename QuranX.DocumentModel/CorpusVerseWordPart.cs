3c065055404f3f1f575cfe1d1c6b32f1e9fb07b4
QuranX <34621974+QuranX@users.noreply.github.com>
2018-12-08 19:17:54 +0000

    Word sub types (#9)

--- QuranX.DocumentModel/CorpusVerseWordPart.cs
+++ QuranX.DocumentModel/CorpusVerseWordPart.cs
@@ -4,6 +4,7 @@
 	{
 		public readonly int Index;
 		public readonly string TypeCode;
+		public readonly int? SubType;
 		public readonly string TypeDescription;
 		public readonly string Root;
 		public readonly string[] Decorators;
@@ -11,11 +12,13 @@
 		public CorpusVerseWordPart(
 			int index,
 			string typeCode,
+			int? subType,
 			string root,
 			string[] decorators)
 		{
 			this.Index = index;
 			this.TypeCode = typeCode;
+			this.SubType = subType;
 			this.TypeDescription = WordTypes.Values[typeCode];
 			this.Root = root;
 			this.Decorators = decorators;
