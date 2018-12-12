821d81f93a556b3db5f0dfa9df459f19e1cbcd3e
QuranX <34621974+QuranX@users.noreply.github.com>
2018-12-12 20:20:10 +0000

    Feature/caching (#14)
    * Improved caching
    
    * Remove application insights

--- QuranX.Web/Controllers/AboutController.cs
+++ QuranX.Web/Controllers/AboutController.cs
@@ -2,6 +2,7 @@
 
 namespace QuranX.Web.Controllers
 {
+	[OutputCache(Duration = Consts.CacheTimeInSeconds, NoStore = Consts.CacheTimeInSeconds == 0)]
 	public class AboutController : Controller
 	{
 		public ActionResult Index()
--- QuranX.Web/Controllers/HelpController.cs
+++ QuranX.Web/Controllers/HelpController.cs
@@ -2,6 +2,7 @@
 
 namespace QuranX.Web.Controllers
 {
+	[OutputCache(Duration = Consts.CacheTimeInSeconds, NoStore = Consts.CacheTimeInSeconds == 0)]
 	public class HelpController : Controller
 	{
 		public ActionResult Index()
