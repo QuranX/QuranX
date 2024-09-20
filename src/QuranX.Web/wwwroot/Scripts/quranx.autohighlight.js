5f83aa40e5590b0f50b2684206baad4507b17b67
QuranX <QuranX.com@gmail.com>
2024-09-20 11:20:09 +0100

    Fix translations

--- src/QuranX.Web/wwwroot/Scripts/quranx.autohighlight.js
+++ src/QuranX.Web/wwwroot/Scripts/quranx.autohighlight.js
@@ -1,13 +1,15 @@
-﻿    function getParameterByName(name) {
-        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-            results = regex.exec(location.search);
-        return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-    }
+﻿function getParameterByName(name) {
+	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+		results = regex.exec(location.search);
+	return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
 
-    $(function () {
-        var hl = getParameterByName("hl");
-        if (hl != null) {
-            $(".highlightable").highlight(hl.split(","));
-        }
-    });
+console.log('a');
+$(function () {
+	console.log('b');
+	var hl = getParameterByName("hl");
+	if (hl != null) {
+		$(".highlightable").highlight(hl.split(","));
+	}
+});
--- src/QuranX.Web/wwwroot/Scripts/quranx.verse-translations.js
+++ src/QuranX.Web/wwwroot/Scripts/quranx.verse-translations.js
@@ -10,39 +10,44 @@
 		hiddenCodeElem.show();
 	}
 }
-var quranXShowAllTranslations = (window.location.search || "").toLowerCase().indexOf("alltranslations=y") > -1;
-// Buttons to show translations
-$(".verse__translation-hidden-item")
-	.on("click", function (ev) {
-		const elem = $(ev.target);
-		const translatorCode = elem.data("translator-code");
-		window.localStorage.setItem("show-" + translatorCode, true);
-		toggleTranslation(translatorCode);
-	});
-// Translations
-$(".verse__translation>dt")
-	.each(function (index, elem) {
-		elem = $(elem.parentElement);
-		const translatorCode = elem.data("translator-code");
-		const useDefaultTranslationsKey = "useDefaultTranslations";
-		const useDefaultTranslations = window.localStorage.getItem(useDefaultTranslationsKey);
-		// If this is the first visit
-		if (useDefaultTranslations !== "N") {
-			// Hide the translation if not one of the defaults to show
-			const defaultTranslations = ["AR", "Pickthall", "SahihIntl", "YusufAli"];
-			for (var translationIndex = 0; translationIndex < defaultTranslations.length; translationIndex++) {
-				const translatorCode = defaultTranslations[translationIndex];
-				window.localStorage.setItem("show-" + translatorCode, true);
+
+console.log('1');
+$(function () {
+	console.log('2');
+	var quranXShowAllTranslations = (window.location.search || "").toLowerCase().indexOf("alltranslations=y") > -1;
+	// Buttons to show translations
+	$(".verse__translation-hidden-item")
+		.on("click", function (ev) {
+			const elem = $(ev.target);
+			const translatorCode = elem.data("translator-code");
+			window.localStorage.setItem("show-" + translatorCode, true);
+			toggleTranslation(translatorCode);
+		});
+	// Translations
+	$(".verse__translation>dt")
+		.each(function (index, elem) {
+			elem = $(elem.parentElement);
+			const translatorCode = elem.data("translator-code");
+			const useDefaultTranslationsKey = "useDefaultTranslations";
+			const useDefaultTranslations = window.localStorage.getItem(useDefaultTranslationsKey);
+			// If this is the first visit
+			if (useDefaultTranslations !== "N") {
+				// Hide the translation if not one of the defaults to show
+				const defaultTranslations = ["AR", "Pickthall", "SahihIntl", "YusufAli"];
+				for (var translationIndex = 0; translationIndex < defaultTranslations.length; translationIndex++) {
+					const translatorCode = defaultTranslations[translationIndex];
+					window.localStorage.setItem("show-" + translatorCode, true);
+				}
+				window.localStorage.setItem(useDefaultTranslationsKey, "N");
 			}
-			window.localStorage.setItem(useDefaultTranslationsKey, "N");
-		}
-		if (!quranXShowAllTranslations && !window.localStorage.getItem("show-" + translatorCode)) {
-			toggleTranslation(translatorCode, true);
-		};
-	})
-	.on("click", function (ev) {
-		const elem = $(ev.target.parentElement);
-		const translatorCode = elem.data("translator-code");
-		window.localStorage.removeItem("show-" + translatorCode, true);
-		toggleTranslation(translatorCode);
-	});
+			if (!quranXShowAllTranslations && !window.localStorage.getItem("show-" + translatorCode)) {
+				toggleTranslation(translatorCode, true);
+			};
+		})
+		.on("click", function (ev) {
+			const elem = $(ev.target.parentElement);
+			const translatorCode = elem.data("translator-code");
+			window.localStorage.removeItem("show-" + translatorCode, true);
+			toggleTranslation(translatorCode);
+		});
+});
