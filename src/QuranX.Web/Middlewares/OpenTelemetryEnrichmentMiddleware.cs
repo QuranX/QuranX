498767f8d85030eb09cac59921dd0605db0d505c
QuranX <QuranX.com@gmail.com>
2024-09-21 13:21:56 +0100

    Add querystring to open telemetry

--- src/QuranX.Web/Middlewares/OpenTelemetryEnrichmentMiddleware.cs
+++ src/QuranX.Web/Middlewares/OpenTelemetryEnrichmentMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Primitives;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -17,12 +18,18 @@ public class OpenTelemetryEnrichmentMiddleware
 
 	public async Task InvokeAsync(HttpContext httpContext)
 	{
-		Activity currentActivity = System.Diagnostics.Activity.Current;
+		Activity currentActivity = Activity.Current;
 		if (currentActivity != null)
 		{
+			// Add route data to activity tags
 			RouteData routeData = httpContext.GetRouteData();
 			foreach (KeyValuePair<string, object> routeValue in routeData.Values)
 				currentActivity.SetTag($"http.route.{routeValue.Key}", routeValue.Value?.ToString());
+
+			// Add query string values to activity tags
+			IQueryCollection queryString = httpContext.Request.Query;
+			foreach (KeyValuePair<string, StringValues> query in queryString)
+				currentActivity.SetTag($"http.query.{query.Key}", query.Value.ToString());
 		}
 
 		await Next(httpContext);
