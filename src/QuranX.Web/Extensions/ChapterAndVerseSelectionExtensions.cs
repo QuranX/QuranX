e53780cda9ac19384bce6b211d2a08fd3301a7f5
QuranX <QuranX.com@gmail.com>
2026-01-23 11:56:24 +0000

    Fix meta description for Qur'an verses

---
+++ src/QuranX.Web/Extensions/ChapterAndVerseSelectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using QuranX.Web.Models;
+
+namespace QuranX.Web.Extensions
+{
+	public static class ChapterAndVerseSelectionExtensions
+	{
+		public static string ToDisplayText(this IEnumerable<ChapterAndVerseSelection> selections)
+		{
+			var items = new List<string>();
+			foreach(var selection in selections)
+			{
+				int firstVerse = selection.Verses.Min(x => x.VerseNumber);
+				int lastVerse = selection.Verses.Max(x => x.VerseNumber);
+				string lastVerseText = null;
+				if (firstVerse != lastVerse)
+					lastVerseText = $"-{lastVerse}";
+				string text = $"{selection.Chapter.EnglishName} {selection.Chapter.ChapterNumber}.{firstVerse}{lastVerseText}";
+				items.Add(text);
+			}
+			return string.Join(", ", items);
+		}
+	}
+}
